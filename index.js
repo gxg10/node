1f61f9a81d8306bcfdc920589872853801656b3e
George Brande <george.brande@gmail.com>
2018-08-08 11:12:55 +0300

    node init

---
+++ index.js
@@ -0,0 +1,25 @@
+var rect = require('./rectangle');
+
+function solveRect(l,b) {
+	console.log("solving for rectangle with l= " + l + "and b = " + b);
+
+	rect(l,b,(err, rectangle) => {
+		if (err) {
+			console.log("Error", err.message);
+		}
+
+		else {
+			console.log("the area of the rectangle of dimensions l = " + l + "and b = " + b + "is " 
+				+ rectangle.area());
+			console.log("the perimeter of rectangle of dimensions l = "+ l + "and b = " + b + " is"
+				+ rectangle.perimeter());
+		}
+	});
+
+	console.log("this statement is after the call to rect()");
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
